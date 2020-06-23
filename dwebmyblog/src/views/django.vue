619fa126bf5b87682aa9f707f2d2e7f20246a4c4
Rik <sdpigpig@qq.com>
2020-06-23 20:25:21 +0800

    ID34

--- dwebmyblog/src/views/django.vue
+++ dwebmyblog/src/views/django.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="userlit">
-    <div class="user">
-      <img src="../assets/logo.svg" alt />
-      <p>test123</p>
+    <div v-for="item in imglist" :key="item.pk" class="user">
+      <img :src="'https://api.dweb.club/'+item.pic" alt />
+      <p>{{  item.title  }}</p>
     </div>
     <div class="user">
       <img src="../assets/logo.svg" alt />
@@ -16,7 +16,30 @@
 </template>
 
 <script>
-export default {};
+import axios from 'axios'
+export default {
+    data(){
+        return{
+            imglist:[]
+        }
+    },
+    //用户在看到网页之前,最后Vue提供的一次函数执行
+    mounted() {
+        this.getData()
+    },
+    methods: {
+        getData(){
+            axios({
+                url:'http://127.0.0.1:9000/api/',
+                type:'json',
+                method:'get'
+            }).then((res)=>{
+                console.log(res)
+                // this.imglist = res.data.newsdata
+            })
+        }
+    },
+};
 </script>
 
 <style>
